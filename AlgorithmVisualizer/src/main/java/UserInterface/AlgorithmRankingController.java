b5968bdaf88af4149f6df7f210303f4f793cda38
edjob <ed.jobe@yahoo.com>
2023-04-11 12:05:43 -0400

    Quiz class changes

---
+++ AlgorithmVisualizer/src/main/java/UserInterface/AlgorithmRankingController.java
@@ -0,0 +1,23 @@
+package UserInterface;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AlgorithmRankingController implements Initializable {
+
+
+    /**
+     * Initializes the UI elements.
+     * @param url url
+     * @param resourceBundle srBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+
+}
